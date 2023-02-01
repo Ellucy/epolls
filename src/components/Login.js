b1efdff1283aff7eac4ee88ba57be79ab7a52200
Eleri Mets <eleri.apsolon@gmail.com>
2023-02-01 13:01:21 +0200

    Design visually log in view

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,43 +1,72 @@
 import { useNavigate, useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { connect } from 'react-redux';
 
 function Login({ users }) {
     const navigate = useNavigate();
     const { pathname } = useLocation();
+    const [selectedUserId, setSelectedUserId] = useState("none");
+    const [selectedUser, setSelectedUser] = useState({});
+
+    useEffect(() => {
+        if (users[selectedUserId]) {
+            setSelectedUser(users[selectedUserId]);
+            return
+        }
+        setSelectedUser({});
+
+    }, [selectedUserId, users]);
 
     return (
         <div className="App padding10">
-            <select
-                value="none"
-                className="padding10"
-                onChange={(event) => {
-                    navigate(
-                        pathname,
-                        {
-                            state: { userId: event.target.value },
-                            replace: true
-                        }
-                    );
-                }}
-            >
-                <option value="none" disabled>
-                    Select active user...
-                </option>
-                {Object.values(users).map((u) => (
-                    <option
-                        key={u.id}
-                        value={u.id}
-                    >
-                        {u.name}
+            <div className="signin-container">
+                <div className="login-header">Employee Polls</div>
+                <select
+                    value={selectedUserId}
+                    className="login-padding10"
+                    onChange={(e) => {
+                        setSelectedUserId(e.target.value)
+                    }}
+                >
+                    <option value="none" disabled>
+                        Select user...
                     </option>
-                ))}
-            </select>
-        </div>
+                    {Object.values(users).map((u) => (
+                        <option
+                            key={u.id}
+                            value={u.id}
+                        >
+                            {u.name}
+                        </option>
+                    ))}
+                </select>
+                <div>
+                    {selectedUser?.avatarURL && (
+                        <img src={selectedUser.avatarURL} alt={`Avatar of ${selectedUser.id}`} className="big-avatar" />
+                    )}
+                </div>
+                <button
+                    className="submit-button"
+                    disabled={selectedUserId === "none"}
+                    onClick={() => {
+                        navigate(
+                            pathname,
+                            {
+                                state: { userId: selectedUserId },
+                                replace: true
+                            }
+                        );
+                    }}
+                >
+                    Submit
+                </button>
+            </div>
+        </div >
     );
 }
+
 const mapStateToProps = ({ users }) => ({
-    users: users,
+    users,
 });
 
 export default connect(mapStateToProps)(Login);
