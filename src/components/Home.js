2424cffa130ffebbe4946aa68d252a432a22be71
Eleri Mets <eleri.apsolon@gmail.com>
2023-02-02 12:45:58 +0200

    Switch between answered and new polls in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,54 +1,72 @@
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 import WithAuthCheck from './WithAuthCheck';
 import TopBar from './TopBar';
 import { formatDate } from '../utils'
 
 function Home({ userId, newQuestions, answeredQuestions }) {
     const navigate = useNavigate();
+    const [isNewPollVisible, setIsNewPollVisible] = useState(true);
     return (
         <WithAuthCheck>
             <TopBar userId={userId} />
             <div className="App">
-                <div className='new-done-polls'>
-                    <div className='poll-container'>New questions</div>
-                    <div className="flex-container">
-                        {newQuestions.map((q) => (
-                            <div key={q.id} className="question-block">
-                                <div className="font-weight-bold">{q.author}</div>
-                                <div className="font-size-smaller">{formatDate(q.timestamp)}</div>
-                                <button
-                                    className='home-btn'
-                                    onClick={() => {
-                                        navigate(`/questions/${q.id}`, { state: { userId } });
-                                    }}
-                                >
-                                    Show
-                                </button>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                <button
+                    className="submit-button"
+                    onClick={() => {
+                        setIsNewPollVisible(!isNewPollVisible)
+                    }}
+                >
+                    {isNewPollVisible
+                        ? ("Show answered polls")
+                        : ("Show new polls")}
+                </button>
 
-                <div className='new-done-polls'>
-                    <div className='poll-container'>Done</div>
-                    <div className="flex-container">
-                        {answeredQuestions.map((q) => (
-                            <div key={q.id} className="question-block">
-                                <div className="font-weight-bold">{q.author}</div>
-                                <div className="font-size-smaller">{formatDate(q.timestamp)}</div>
-                                <button
-                                    className='home-btn'
-                                    onClick={() => {
-                                        navigate(`/questions/${q.id}`, { state: { userId } });
-                                    }}
-                                >
-                                    Show
-                                </button>
-                            </div>
-                        ))}
+                {isNewPollVisible && (
+                    <div className='new-done-polls'>
+                        <div className='poll-container'>New questions</div>
+                        <div className="flex-container">
+                            {newQuestions.map((q) => (
+                                <div key={q.id} className="question-block">
+                                    <div className="font-weight-bold">{q.author}</div>
+                                    <div className="font-size-smaller">{formatDate(q.timestamp)}</div>
+                                    <button
+                                        className='home-btn'
+                                        onClick={() => {
+                                            navigate(`/questions/${q.id}`, { state: { userId } });
+                                        }}
+                                    >
+                                        Show
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                </div>
+                )}
+
+                {!isNewPollVisible && (
+                    <div className='new-done-polls'>
+                        <div className='poll-container'>Done</div>
+                        <div className="flex-container">
+                            {answeredQuestions.map((q) => (
+                                <div key={q.id} className="question-block">
+                                    <div className="font-weight-bold">{q.author}</div>
+                                    <div className="font-size-smaller">{formatDate(q.timestamp)}</div>
+                                    <button
+                                        className='home-btn'
+                                        onClick={() => {
+                                            navigate(`/questions/${q.id}`, { state: { userId } });
+                                        }}
+                                    >
+                                        Show
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+
             </div>
         </WithAuthCheck>
     );
